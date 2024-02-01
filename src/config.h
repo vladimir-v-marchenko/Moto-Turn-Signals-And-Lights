b1aab5293b0b26f0799a229a4e0047b326219ee5
Volodymyr V. Marchenko <volodymyr_marchenko@icloud.com>
2024-02-01 17:07:40 +0200

    Update
    Signed-off-by: Volodymyr V. Marchenko <volodymyr_marchenko@icloud.com>

--- src/config.h
+++ src/config.h
@@ -15,73 +15,7 @@
 
 #define CONTROL_PANEL_LIGHT 11
 
-#define Set_SIG_NONE() digitalWrite(RH_LIGHT, LOW);digitalWrite(LH_LIGHT, LOW);digitalWrite(EMERGENCY_HEAD_LIGHT, LOW);
-#define Set_SIG_LEFT() digitalWrite(RH_LIGHT, LOW);digitalWrite(LH_LIGHT, HIGH);digitalWrite(EMERGENCY_HEAD_LIGHT, LOW);
-#define Set_SIG_RIGHT() digitalWrite(RH_LIGHT, HIGH);digitalWrite(LH_LIGHT, LOW);digitalWrite(EMERGENCY_HEAD_LIGHT, LOW);
-#define Set_SIG_EMERGENCY() digitalWrite(RH_LIGHT, HIGH);digitalWrite(LH_LIGHT, HIGH);digitalWrite(EMERGENCY_HEAD_LIGHT, HIGH;
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
-byte _leftTop[8] = {
-  0x00,
-  0x00,
-  0x00,
-  0x01,
-  0x03,
-  0x07,
-  0x0F,
-  0x1F
-};
-
-byte _leftBottom[8] = {
-  0x1F,
-  0x0F,
-  0x07,
-  0x03,
-  0x01,
-  0x00,
-  0x00,
-  0x00
-};
-
-byte _rightTop[8] = {
-  0x00,
-  0x00,
-  0x00,
-  0x10,
-  0x18,
-  0x1C,
-  0x1E,
-  0x1F
-};
-byte _rightBottom[8] = {
-  0x1F,
-  0x1E,
-  0x1C,
-  0x18,
-  0x10,
-  0x00,
-  0x00,
-  0x00
-};
-
-byte _baseTop[8] = {
-  0x00,
-  0x00,
-  0x00,
-  0x00,
-  0x00,
-  0x1F,
-  0x1F,
-  0x1F
-};
-
-
-byte _baseBottom[8] = {
-  0x1F,
-  0x1F,
-  0x1F,
-  0x00,
-  0x00,
-  0x00,
-  0x00,
-  0x00
-};
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,30 @@
-//#include <SPI.h>
+#include <Arduino.h>
 #include <Wire.h> 
 #include <OneWire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include <RTClib.h>
-#include <DallasTemperature.h>
-#include "SwitchSignal.h"
-#include "config.h"
+// Flasher Blink delay
+#define BLINK_INTERVAL      500
+#define BEEP_FREQUENCY     1000
+// D12 Buzzer connection pin
+#define BUZZER_PIN           12
+// Emergency Signal head light pin
+#define EMERGENCY_HEAD_LIGHT 11
+// Right Signal light pin
+#define RH_LIGHT             10
+// Left Signal light pin 
+#define LH_LIGHT              9
+// Right Signal switch pin 
+#define RH_SWITCH_PIN         8
+// Left Signal switch pin 
+#define LH_SWITCH_PIN         7
+#define CONTROL_PANEL_LIGHT  11
+// Display W:H
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define SSD1306_WHITE   1
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
-
-#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-RTC_DS1307 rtc;
-
-void displayTextCenter(String text) {
-  int16_t x1;
-  int16_t y1;
-  uint16_t width;
-  uint16_t height;
-
-  display.getTextBounds(text, 0, 0, &x1, &y1, &width, &height);
-
-  // display on horizontal and vertical center
-  display.clearDisplay(); // clear display
-  display.setCursor((SCREEN_WIDTH - width) / 2, (SCREEN_HEIGHT - height) / 2);
-  display.println(text); // text to display
-  display.display();
-}
-void displayLeftSig(void) {
-  display.clearDisplay();
-  display.fillTriangle(62, 0, 0, 32, 62, 64, WHITE);
-  display.display();
-}
-
-void displayRightSig(void) {
-  display.clearDisplay();
-  display.fillTriangle(66, 0, 128, 32, 66, 64, WHITE);
-  display.display();
-}
-
-void displayEmergencySig(void) {
-  display.clearDisplay();
-  display.fillTriangle(62, 0, 0, 32, 62, 64, WHITE);
-  display.fillTriangle(66, 0, 128, 32, 66, 64, WHITE);
-  display.display();  
-}
-
-void displayClear(void) {
-  display.clearDisplay();
-  display.display();
-}
+Adafruit_SSD1306 display = Adafruit_SSD1306(128, 64, &Wire, -1);
 
 enum class State {
   NONE,
@@ -63,172 +36,180 @@ enum class State {
 };
 
 volatile State signal_state = State::NONE;
-byte gear = 0;
+
+#include <SwitchSignal.h>
+
 SwitchSignal LHswitch;
 SwitchSignal RHswitch;
-
-void handleLHPress(const byte newState, const unsigned long interval, const byte whichPin)
-{
-  // switch down?
-  if (newState == LOW) {
-     switch (signal_state) {
-       // if other switch down, switch to warning mode
-       case State::RH_DOWN:
-         signal_state = State::EMERGENCY;
-         break;
-       // if already on or warning signal, turn all off
-       case State::LH_LIGHT_ON:
-       case State::EMERGENCY:
-         signal_state = State::NONE;
-        break;
-       // otherwise switch is now down, but not yet released
-       default:
-         signal_state = State::LH_DOWN;
-         break;
-       }  // end of switch
-     return;
-     }  // end of LH switch down
-  // switch must be up
-     if (signal_state == State::LH_DOWN) { // if down, switch to down-and-released mode
-      signal_state = State::LH_LIGHT_ON;
-     }
-} // end of handleLHPress
-
-void handleRHPress(const byte newState,
-              const unsigned long interval,
-              const byte whichPin)
-{
-     // switch down?
-     if (newState == LOW) {
-     switch(signal_state) {
-       // if other switch down, switch to warning mode
-       case State::LH_DOWN:
-         signal_state = State::EMERGENCY;
-         break;
-       // if already on or warning signal, turn all off
-       case State::RH_LIGHT_ON:
-       case State::EMERGENCY:
-         signal_state = State::NONE;
-         break;
-       // otherwise switch is now down, but not yet released
-       default:
-         signal_state = State::RH_DOWN;
-         break;
-       }  // end of switch
-     return;
-     }  // end of RH switch down
-  // switch must be up
-     if (signal_state == State::RH_DOWN) { // if down, switch to down-and-released mode
-     signal_state = State::RH_LIGHT_ON;
-     }
-} // end of handleRHPress
 
 unsigned long lastBlink;
 bool onCycle;
 
-void blinkLights() {
-  lastBlink = millis();
-  onCycle = !onCycle;
-  // default to off
-/*  if (signal_state == State::LH_LIGHT_ON) cleanLeftSig();
-  if (signal_state == State::RH_LIGHT_ON) cleanRightSig();
-  if (signal_state == State::EMERGENCY) cleanEmergency();
-*/
-  //lcd.clear();
+int16_t x1;
+int16_t y1;
+uint16_t width;
+uint16_t height;
+int8_t sig_count = -1;
 
-  // digitalWrite(LED_BUILTIN, LOW);
+void displayLeftSig(void) {
+  Serial.println(F("LEFT"));
+  digitalWrite(LH_LIGHT, HIGH);
+  tone(BUZZER_PIN, BEEP_FREQUENCY);
+  display.getTextBounds(F("LEFT"), 0, 0, &x1, &y1, &width, &height);
+  display.clearDisplay();
+  display.setCursor((SCREEN_WIDTH - width) / 2, 0);     
+  display.print(F("LEFT"));
+  display.fillTriangle(32, 40, 96, 16, 96, 64, SSD1306_WHITE);
+  display.display();
+}
+
+void displayRightSig(void) {
+  Serial.println(F("RIGHT"));
+  digitalWrite(RH_LIGHT, HIGH);
+  tone(BUZZER_PIN, BEEP_FREQUENCY);
+  display.clearDisplay();
+  display.getTextBounds(F("RIGHT"), 0, 0, &x1, &y1, &width, &height);
+  display.setCursor((SCREEN_WIDTH - width) / 2, 0);   
+  display.print(F("RIGHT"));
+  display.fillTriangle(32, 16, 96, 40, 32, 64, SSD1306_WHITE);
+  display.display();
+}
+
+void displayEmergencySig(void) {
+  Serial.println(F("EMERGENCY"));
+  digitalWrite(LH_LIGHT, HIGH);
+  digitalWrite(RH_LIGHT, HIGH);
+  digitalWrite(EMERGENCY_HEAD_LIGHT, HIGH);
+  tone(BUZZER_PIN, BEEP_FREQUENCY);
+  display.clearDisplay();
+  display.getTextBounds(F("EMERGENCY"), 0, 0, &x1, &y1, &width, &height);
+  display.setCursor((SCREEN_WIDTH - width) / 2, 0);
+  display.print(F("EMERGENCY"));
+  //display.fillTriangle(62, 16, 0, 48, 62, 64, WHITE);
+  //display.fillTriangle(66, 16, 128, 48, 66, 64, WHITE);
+  display.display();  
+}
+
+void displayClear(void) {
   digitalWrite(LH_LIGHT, LOW);
   digitalWrite(RH_LIGHT, LOW);
   digitalWrite(EMERGENCY_HEAD_LIGHT, LOW);
   noTone(BUZZER_PIN);
+  display.clearDisplay();
+  display.display();
+}
+
+void blinkLights() {
+  lastBlink = millis();
+  onCycle = !onCycle;
+
   // every second time, turn them all off
   if (!onCycle) {
+     displayClear();
      return;
   }
   // blink light
   switch (signal_state) {
     case State::NONE:
+      digitalWrite(LH_LIGHT, LOW);
+      digitalWrite(RH_LIGHT, LOW);
+      digitalWrite(EMERGENCY_HEAD_LIGHT, LOW);
       break;
     case State::LH_DOWN:
     case State::LH_LIGHT_ON:
       displayLeftSig();
-      //digitalWrite(LED_BUILTIN, HIGH);
-      digitalWrite(LH_LIGHT, HIGH);
-      digitalWrite(EMERGENCY_HEAD_LIGHT, LOW);
-      tone(BUZZER_PIN, BEEP_FREQUENCY);
       break;
     case State::RH_DOWN:
     case State::RH_LIGHT_ON:
       displayRightSig();
-      //digitalWrite(LED_BUILTIN, HIGH);
-      digitalWrite(RH_LIGHT, HIGH);
-      digitalWrite(EMERGENCY_HEAD_LIGHT, LOW);
-      tone(BUZZER_PIN, BEEP_FREQUENCY);
       break;
     case State::EMERGENCY:
       displayEmergencySig();
-      //digitalWrite(LED_BUILTIN, HIGH);
-      digitalWrite(LH_LIGHT, HIGH);
-      digitalWrite(RH_LIGHT, HIGH);
-      digitalWrite(EMERGENCY_HEAD_LIGHT, HIGH);
-      tone(BUZZER_PIN, BEEP_FREQUENCY);
       break;
     }  // end of switch on state
 }  // end of blinkLights
 
-void dashboard() {
 
-}
-// ------------------------- SETUP -------------------------
-void setup() {
-  Serial.begin (9600);
+void i2c_scanner()
+{
+  byte error, address;
+  int nDevices;
 
-  // initialize OLED display with address 0x3C for 128x64
-  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    while (true);
+  Serial.println("Scanning...");
+
+  nDevices = 0;
+  for(address = 1; address < 127; address++ )
+  {
+    // The i2c_scanner uses the return value of
+    // the Write.endTransmisstion to see if
+    // a device did acknowledge to the address.
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+
+    if (error == 0)
+    {
+      Serial.print("I2C device found at address 0x");
+      if (address<16)
+        Serial.print("0");
+      Serial.print(address,HEX);
+      Serial.println("  !");
+
+      nDevices++;
+    }
+    else if (error==4)
+    {
+      Serial.print("Unknown error at address 0x");
+      if (address<16)
+        Serial.print("0");
+      Serial.println(address,HEX);
+    }
   }
+  if (nDevices == 0)
+    Serial.println("No I2C devices found\n");
+  else
+    Serial.println("done\n");
 
-  // displayClear();
-    Serial.println(F("Loading..."));
-    displayTextCenter("Loading...");
-    delay(1000);
-  //pinMode(LED_BUILTIN, OUTPUT);
-  //digitalWrite(LED_BUILTIN, LOW);
- 
+  delay(5000);           // wait 5 seconds for next scan
+}// ------------------------- SETUP -------------------------
+void setup() {
+  Wire.begin();
+  Serial.begin(9600);
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+
+  Serial.println(F("Initialize: Display"));
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor((SCREEN_WIDTH - width) / 2, 0); 
+  display.clearDisplay();
+  Serial.print(F("Loading"));
+  display.println(F("Loading"));
+  display.display();
+  
+  //i2c_scanner();
+
   LHswitch.begin(LH_SWITCH_PIN, handleLHPress);
   pinMode(LH_LIGHT, OUTPUT);
   digitalWrite(LH_LIGHT, LOW);
-
 
   RHswitch.begin(RH_SWITCH_PIN, handleRHPress);
   pinMode(RH_LIGHT, OUTPUT);
   digitalWrite(RH_LIGHT, LOW);
 
-  
   pinMode(EMERGENCY_HEAD_LIGHT, OUTPUT);
   digitalWrite(EMERGENCY_HEAD_LIGHT, LOW);
 
-
-  pinMode(CONTROL_PANEL_LIGHT, OUTPUT);
-  digitalWrite(CONTROL_PANEL_LIGHT, LOW);
-  // for (int i=0; i<256; i++) {
-  //   analogWrite(CONTROL_PANEL_LIGHT, i);
-  //   if (i%10) { lcd.print('.'); }
-  //   delay(5);
-  // }
-  
   pinMode(BUZZER_PIN, OUTPUT);
   tone(BUZZER_PIN, BEEP_FREQUENCY);
   delay(500);
   noTone(BUZZER_PIN);
 
-  // lcd.setCursor(5,0);
-  // lcd.print("DONE");
-
-  //delay(5000);
- // digitalWrite(LED_BUILTIN, HIGH);
-
+  Serial.println(F("Ready"));
+  display.clearDisplay();
+  display.setCursor((SCREEN_WIDTH - width) / 2, 0);     
+  display.println(F("Ready"));
+  display.display();
 }  // end of setup
 
 
@@ -236,16 +217,6 @@ void setup() {
 void loop() {
   LHswitch.check();  // check for left signal button presses
   RHswitch.check();  // check for right signal button presses
-
-/*
-  if (digitalRead(HORN_SWITCH_PIN) == HIGH) 
-  {
-    digitalWrite(HORN_SIGNAL, HIGH);
-  } 
-    else {
-      digitalWrite(HORN_SIGNAL, LOW);
-  }
-*/
   if (millis() - lastBlink >= BLINK_INTERVAL) {
     blinkLights();
   }
